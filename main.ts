70967d2f22d5d0eaef94e9dea6fc15b05ae244a4
IvanGuell <146827049+IvanGuell@users.noreply.github.com>
2024-10-28 16:53:03 +0100

    Final

--- main.ts
+++ main.ts
@@ -1,3 +1,32 @@
-basic.forever(function () {
-	
+let rand_number = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Target)
+})
+input.onGesture(Gesture.Shake, function () {
+    rand_number = randint(0, 3)
+    if (rand_number == 1) {
+        basic.showIcon(IconNames.Target)
+    } else if (rand_number == 2) {
+        basic.showIcon(IconNames.Scissors)
+    } else {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Scissors)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
 })
